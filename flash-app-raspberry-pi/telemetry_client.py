fc832053afdf3cbc6b149ed4a0ff76364a92356a
Dave Glover <dglover@microsoft.com>
2020-11-07 22:40:47 +1100

    updated raspberry pi sample

--- flash-app-raspberry-pi/telemetry_client.py
+++ flash-app-raspberry-pi/telemetry_client.py
@@ -4,8 +4,18 @@ import time
 
 
 class Telemetry():
+
+    def get_cpu_temperature(self):
+        temp = 0.0
+        with open('/sys/class/thermal/thermal_zone0/temp') as temperaturefile:
+            try:
+                temp = float(temperaturefile.read()) / 1000
+            except:
+                print('Problem reading CPU Temperature')
+        return temp
+
     def measure(self):
-        retry = 0
+        
 
         # while retry < 2:
         #     try:
@@ -22,7 +32,7 @@ class Telemetry():
             "humidity": random.randrange(50, 90),
             "pressure": random.randrange(905, 1300),
             "timestamp": int(time.time()),
-            "cputemperature": random.randrange(30, 90),
+            "cputemperature": self.get_cpu_temperature(),
         }
 
         return telemetry.get('temperature'), telemetry.get('pressure'), telemetry.get('humidity'), telemetry.get('timestamp'), telemetry.get('cputemperature')
